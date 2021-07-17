10e53646b03ced9b8437c1d9f0c78a3267a818fd
arunbscmdu <arun.baskaran@roanuz.com>
2021-07-17 19:14:54 +0530

    about us page fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,68 +35,6 @@
         font-weight: 700;
       }
     }
-    .objectives {
-      padding: size(40) 0;
-      h3 {
-        font-size: size(24);
-        line-height: size(30);
-        margin: size(20) 0;
-        font-weight: bold;
-        text-align: center;
-        padding-bottom: 20px;
-        color: #e06565;
-      }
-      &-list {
-        display: flex;
-        flex-flow: column;
-        .objective-item {
-          position: relative;
-          margin: 0 auto;
-          margin-bottom: size(20);
-          max-width: size(370);
-          h5 {
-            font-size: var(--common-para-font-size);
-            line-height: var(--common-para-line-height);
-            font-weight: bold;
-            margin-bottom: size(15);
-            text-shadow: 1px 1px 1px #000000ab;
-            text-align: center;
-            position: relative;
-            display: inline-block;
-            padding-bottom: size(5);
-            &:after {
-              content: '';
-              position: absolute;
-              left: 0;
-              right: 0;
-              top: 100%;
-              width: 70%;
-              height: size(4);
-              background: #e06565;
-              margin: 0 auto;
-            }
-          }
-          p {
-            font-size: size(18);
-            line-height: size(24);
-            text-shadow: 1px 1px 1px #000000ab;
-          }
-          img {
-            width: 100%;
-            height: auto;
-          }
-          .box-content {
-            position: absolute;
-            padding: 0 20px;
-            left: 0;
-            right: 0;
-            bottom: 20px;
-            z-index: 2;
-            color: #fff;
-          }
-        }
-      }
-    }
   }
   &__title {
     font-size: var(--common-title-font-size);
@@ -118,17 +56,6 @@
       height: size(4);
       background: var(--secondary-bg);
       margin: 0 auto;
-    }
-  }
-
-  @media screen and (min-width: $breakpoint-lg) {
-    &__content {
-      .objectives {
-        &-list {
-          flex-flow: row;
-          column-gap: size(25);
-        }
-      }
     }
   }
 }
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -28,7 +28,7 @@
         <div class="objectives">
           <h3>OUR OBJECTIVES</h3>
           <div class="objectives-list">
-            <div class="objective-item">
+            <div class="objective-item wow animate__fadeInLeft">
               <div class="box-image">
                 <a href="#" target="_self">
                   <span>
@@ -43,7 +43,7 @@
                 </p>
               </div>
             </div>
-            <div class="objective-item">
+            <div class="objective-item wow animate__fadeInUp">
               <div class="box-image">
                 <a href="#" target="_self">
                   <span>
@@ -58,7 +58,7 @@
                 </p>
               </div>
             </div>
-            <div class="objective-item">
+            <div class="objective-item wow animate__fadeInRight">
               <div class="box-image">
                 <a href="#" target="_self">
                   <span>
@@ -117,6 +117,77 @@ export default {
     display: flex;
     column-gap: size(40);
     margin-top: size(60);
+  }
+}
+
+.objectives {
+  padding: size(40) 0;
+  h3 {
+    font-size: size(24);
+    line-height: size(30);
+    margin: size(20) 0;
+    font-weight: bold;
+    text-align: center;
+    padding-bottom: 20px;
+    color: #e06565;
+  }
+  &-list {
+    display: flex;
+    flex-flow: column;
+    min-height: size(450);
+    .objective-item {
+      position: relative;
+      margin: 0 auto;
+      margin-bottom: size(20);
+      max-width: size(370);
+      h5 {
+        font-size: var(--common-para-font-size);
+        line-height: var(--common-para-line-height);
+        font-weight: bold;
+        margin-bottom: size(15);
+        text-shadow: 1px 1px 1px #000000ab;
+        text-align: center;
+        position: relative;
+        display: inline-block;
+        padding-bottom: size(5);
+        &:after {
+          content: '';
+          position: absolute;
+          left: 0;
+          right: 0;
+          top: 100%;
+          width: 70%;
+          height: size(4);
+          background: #e06565;
+          margin: 0 auto;
+        }
+      }
+      p {
+        font-size: size(18);
+        line-height: size(24);
+        text-shadow: 1px 1px 1px #000000ab;
+      }
+      img {
+        width: 100%;
+        height: auto;
+      }
+      .box-content {
+        position: absolute;
+        padding: 0 20px;
+        left: 0;
+        right: 0;
+        bottom: 20px;
+        z-index: 2;
+        color: #fff;
+      }
+    }
+  }
+
+  @media screen and (min-width: $breakpoint-lg) {
+    &-list {
+      flex-flow: row;
+      column-gap: size(25);
+    }
   }
 }
 </style>
